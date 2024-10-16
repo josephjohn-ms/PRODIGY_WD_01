b4c6e676d24b0e1a8a568d6b673cba9a8be7aae3
John <135991988+josephjohn-ms@users.noreply.github.com>
2024-10-16 07:53:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const navbar = document.getElementById('navbar');
+
+// event listener for the navbar scroll function
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
